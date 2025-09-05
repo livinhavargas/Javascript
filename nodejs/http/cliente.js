9e2e8abab6d6b32bf3e405e5d6bc0b877ac73fb3
Aluno IFC <aluno@lab32-pc01.sombrio.ifc.edu.br>
2025-09-05 10:46:15 -0300

    Versao com modulo http

---
+++ nodejs/http/cliente.js
@@ -0,0 +1,19 @@
+//importa o módulo http
+const http = require('http');
+
+http.get('http://jsonplaceholder.typicode.com/todos/2', (res) => {
+  let data  = '';
+
+  //um pedaço de dado foi recebido 
+  res.on('data', (chunk) => {
+    data += chunk;
+    });
+
+    //a resposta completa foi recebida
+    res.on('end', () => {
+        console.log(JSON.parse(data));
+    });
+
+}).on("error", (err) => {
+    console.log("Error: " + err.message);
+});
