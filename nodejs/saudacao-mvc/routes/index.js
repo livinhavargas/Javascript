34acb91ce2824fec886902224291662de5e2a9ee
Lívia de Vargas da Silva <liviavsilvaaa@gmail.com>
2025-10-14 10:50:34 -0300

    Gênero no form

---
+++ nodejs/saudacao-mvc/routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const siteController = require('../controllers/siteController');
+
+router.get('/', siteController.index);
+router.get('/formulario', siteController.formulario);
+router.post('/saudacao', siteController.saudacao);
+module.exports = router;
