cbf26b04b8822fc4626392fbb3c8752d80ebb31c
Lívia de Vargas <liviavsilvaaa@gmail.com>
2025-10-28 10:41:29 -0300

    Cadastro de produtos

--- nodejs/saudacao-mvc/controllers/produtosController.js
+++ nodejs/saudacao-mvc/controllers/produtosController.js
@@ -5,5 +5,17 @@ module.exports = {
   },
     camisetas: (req, res) => {
     res.sendFile('camisetas.html', { root: './views' });
-    }
-    };
+  },
+
+  formCadastro: (req, res) => {
+    res.sendFile('cadastro.html', { root: './views' });
+  },
+
+  cadastro: (req, res) => {
+    const { id, descricao, quantidade, preco } = req.body;
+    const produtosModel = require('../models/produtosModel');
+    const mensagem = produtosModel.verificaCadastro(id, descricao, quantidade, preco);
+    res.send(mensagem);
+  }
+  
+};
